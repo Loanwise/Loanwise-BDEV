87a93716d4ce42995abc96bbd95a0a3188fc0f57
Deedee <deedeeowoeye@gmail.com>
2023-06-19 20:23:35 +0100

    Update Notification Settings Feature

--- controllers/notification-controller.js
+++ controllers/notification-controller.js
@@ -5,15 +5,14 @@ const getNotification = async (req, res) => {
     try {
         const user = await Users.findOne({_id: req.params.id})
         if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ message: 'User not found' });
         } 
 
         return res.json({
-        inAppNotification: user.inAppNotification,
-        emailNotification: user.emailNotification,
-        notificationSettings: user.notificationSettings,
+            newApplications: user.newApplications,
+            loanRepayments: user.loanRepayments,
+            dueDates: user.dueDates,
         });
-
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error' });
@@ -24,23 +23,28 @@ const getNotification = async (req, res) => {
 const updateNotification = async (req, res) => {
 
     try {
-        const { inAppNotification, emailNotification, notificationSettings } = req.body;
+        const { newApplications, loanRepayments, dueDates } = req.body;
         const updatedUser = await Users.findOneAndUpdate(
-        {_id: req.params.id},
-        {
-            inAppNotification: inAppNotification || false,
-            emailNotification: emailNotification || false,
-            notificationSettings: {
-            newApplications: notificationSettings?.newApplications || false,
-            loanRepayments: notificationSettings?.loanRepayments || false,
-            dueDates: notificationSettings?.dueDates || false,
+            {_id: req.params.id},
+            {
+                newApplications: {
+                    inAppNotification: newApplications?.inAppNotification || false,
+                    emailNotification: newApplications?.emailNotification || false,
+                },
+                loanRepayments: {
+                    inAppNotification: loanRepayments?.inAppNotification || false,
+                    emailNotification: loanRepayments?.emailNotification || false,
+                },
+                dueDates: {
+                    inAppNotification: dueDates?.inAppNotification || false,
+                    emailNotification: dueDates?.emailNotification || false,
+                },
             },
-        },
-        { new: true }
+            { new: true }
         );
         
         if (!updatedUser) {
-        return res.status(400).json({ message: 'User not found' });
+            return res.status(400).json({ message: 'User not found' });
         }
         return res.json({ message: 'Notification settings updated successfully' });
 
--- model/user_notification.js
+++ model/user_notification.js
@@ -3,33 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema({
-    
     name: {
         type: String,
         required: true,
     },
-    inAppNotification: {
-        type: Boolean,
-        default: true,
-    },
-    emailNotification: {
-        type: Boolean,
-        default: false,
-    },
-    notificationSettings: {
-        newApplications: {
+    newApplications: {
+        inAppNotification: {
             type: Boolean,
             default: true,
         },
-        loanRepayments: {
+        emailNotification: {
             type: Boolean,
             default: false,
         },
-        dueDates: {
+    },
+    loanRepayments: {
+        inAppNotification: {
+            type: Boolean,
+            default: true,
+        },
+        emailNotification: {
             type: Boolean,
             default: false,
         },
-    }
+    },
+    dueDates: {
+        inAppNotification: {
+            type: Boolean,
+            default: true,
+        },
+        emailNotification: {
+            type: Boolean,
+            default: false,
+        },
+    },
 });
 
 module.exports = mongoose.model('Users', usersSchema);
