61e0002fc5b016c267ad85bebccd90dd7bfefcbd
NobulPlus <chenko4me@gmai.com>
2023-06-20 18:54:28 +0100

    new modification

--- model/User.js
+++ model/User.js
@@ -17,6 +17,11 @@ const userSchema = new Schema({
         required: true,
         minlength: 6
     },
+    confirmPassword:{
+      type: String,
+      required: true,
+      minlength: 6
+    },
     securityQuestions: [{
         question: {
           type: String,
