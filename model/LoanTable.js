2a6869d9cf7c9769844f1f2042fc09d4d3bfa921
adeleyepaul <adeleyepaul>
2023-07-02 06:23:03 +0100

    portfolio analysis done

--- model/LoanTable.js
+++ model/LoanTable.js
@@ -1,42 +1,81 @@
 const mongoose = require('mongoose');
 
 const loanSchema = new mongoose.Schema({
-  customer_id: String,
-  name: String,
-  Address: String,
-  email: String,
-  phone_number: String,
-  employment_status: String,
-  loan_term: String,
-  verification_by_loan_company: String,
-  loan_status: String,
-  loan_purpose: String,
-  application_type: String,
-  total_years_of_employment: String,
-  annual_income: Number,
-  income_debt_ratio: Number,
-  no_of_openCreditLine: Number,
-  credit_utilization_rate: Number,
-  no_of_mortgageAccount: Number,
-  employment_status__1: String,
-  credit_score: Number,
-  Requested: Number,
-  Disbursed: Number,
-  loan_duration: String,
-  repayment_method: String,
-  interest_rate: Number,
-  due_date: String,
-  loan_status: String,
-  loan_status_2: String,
-  late_fee: Number,
-  Refunded: String,
-  Default: String,
-  Category: String,
-  first_repayment: String,
-  second_repayment: String,
-  loan_received: String,
-  loan_reviewed: String,
-  loan_disbursed: String
+  customer_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  alternativeEmail: {
+    type: String,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  bvn: {
+    type: String,
+    required: true,
+  },
+  employmentData: {
+    currentEmployer: {
+      type: String,
+    },
+    currentRole: {
+      type: String,
+    },
+    annualIncome: {
+      type: Number,
+    },
+    totalYearsOfEmployment: {
+      type: Number,
+    },
+    incomeDebitRatio: {
+      type: Number,
+    },
+    openCreditLines: {
+      type: Number,
+    },
+    creditUtilizationRate: {
+      type: Number,
+    },
+    mortgageAccounts: {
+      type: Number,
+    },
+  },
+  loanData: {
+    loanPurpose: {
+      type: String,
+    },
+    loanTerm: {
+      type: String,
+    },
+    requestedAmount: {
+      type: Number,
+    },
+    loanCompanyVerification: {
+      type: Boolean,
+    },
+    applicationType: {
+      type: String,
+    },
+  },
 });
 
 const Loan = mongoose.model('LoanTable', loanSchema);
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { borrowers_details } = require("../controllers/borrower-controller")
+const { borrowers_details, saveEmploymentData } = require("../controllers/borrower-controller")
 const { signup, login, verifyToken, getUser, refreshToken, forgetPassword, resetPassword, verifySignup, recoveryAccount, securityQuestions } = require("../controllers/user-controller");
 const { getNotification, updateNotification } = require("../controllers/notification-controller");
 
@@ -13,11 +13,10 @@ router.get('/refresh', refreshToken, verifyToken, getUser);
 router.post("/forget-password", forgetPassword);
 router.post("/recovery-account", recoveryAccount)
 router.post("/reset-password", resetPassword)
-router.post("/borrow", borrowers_details)
+// router.post("/borrow", borrowers_details)
 router.put("/:userId/security-question", securityQuestions)
 
 router.post("/reset-password", resetPassword);
-router.post("/borrowers-analysis", borrowers_details)
 router.get("/settings/notification/:id", getNotification);
 router.put("/settings/notification/:id", updateNotification);
 
@@ -34,5 +33,9 @@ const { getLoanTable, loanTable, addLoanData } = require('../controllers/loan-co
 router.get("/loan-table", getLoanTable)
 router.post("/create-loan", loanTable)
 // router.post("/add-loan", addLoanData)
+
+//router portfolio analysis
+router.post("/borrowers-details", borrowers_details)
+router.post("/employment-details", saveEmploymentData)
 
 module.exports = router
