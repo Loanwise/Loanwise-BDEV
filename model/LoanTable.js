6870b28b09232065d3094d46455234955a7fd661
unknown <adeleyepaul2@gmail.com>
2023-07-14 14:36:08 +0100

    forget-reset-password pushed

--- model/LoanTable.js
+++ model/LoanTable.js
@@ -10,69 +10,139 @@ const loanSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  address: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  alternativeEmail: {
-    type: String,
+  dateOfBirth: {
+    type: Date,
+    // required: true,
   },
   phoneNumber: {
     type: String,
     // required: true,
   },
-  dateOfBirth: {
-    type: Date,
-    // required: true,
-  },
-  bvn: {
+  address: {
     type: String,
     // required: true,
   },
-  employmentData: {
-    currentEmployer: {
-      type: String,
-    },
+  email: {
+    type: String,
+    unique: true,
+    // required: true,
+  },
+  phone_number: {
+    type: Number,
+    // required: true,
+  },
+  employment_status: {
+    type: String,
+  },
+  employment_status_1: {
+    type: String,
+  },
+  credit_score: {
+    type: Number,
+  },
+  Disbursed: {
+    type: Number,
+  },
+  loan_duration: {
+    type: Number,
+  },
+  repayment_method: {
+    type: String,
+  },
+  interest_rate: {
+    type: Number,
+  },
+  due_date: {
+    type: String,
+  },
+  loan_status: {
+    type: String,
+  },
+  loan_status_2: {
+    type: String,
+  },
+  late_fee: {
+    type: String,
+  },
+  Refunded: {
+    type: Number,
+  },
+  Default: {
+    type: Number,
+  },
+  bvn: {
+    type: String,
+  },
+  Category: {
+    type: String,
+  },
+  first_repayment: {
+    type: Number,
+  },
+  second_repayment: {
+    type: Number,
+  },
+  loan_received: {
+    type: String,
+  },
+  loan_reviewed: {
+    type: String,
+  },
+  loan_disbursed: {
+    type: String,
+  },
+  EmploymentData: {
     currentRole: {
       type: String,
+      // required: true,
     },
-    Annual_Income: {
-      type: Number,
-    },
-    Total_Years_of_Employment: {
-      type: Number,
-    },
-    Income_Debt_Ratio: {
-      type: Number,
-    },
-    No_of_Open_Credit_Lines: {
-      type: Number,
-    },
-    Credit_Utilization_Rate: {
-      type: Number,
-    },
-    No_of_Mortgage_Account: {
-      type: Number,
-    },
-    Loan_Purpose: {
+    currentEmployer: {
       type: String,
+      // required: true,
     },
     Loan_Term: {
       type: String,
-    },
-    requestedAmount: {
-      type: Number,
+      // required: true,
     },
     Verification_by_Loan_Company: {
-      type: Boolean,
+      type: String,
+      // required: true,
+    },
+    Loan_Purpose: {
+      type: String,
+      // required: true,
     },
     Application_Type: {
       type: String,
+      // required: true,
+    },
+    Total_years_of_employment: {
+      type: Number,
+      // required: true,
+    },
+    Annual_Income: {
+      type: Number,
+      // required: true,
+    },
+    Income_Debt_Ratio: {
+      type: Number,
+      // required: true,
+    },
+    No_of_Mortgage_Account: {
+      type: Number,
+      // required: true,
+    },
+    Credit_Utilization_Rate: {
+      type: Number,
+      // required: true,
+    },
+    No_of_Open_Credit_Lines: {
+      type: Number,
+      // required: true,
+    },
+    requestedAmount: {
+      type: Number,
+      // required: true,
     },
   },
 });
